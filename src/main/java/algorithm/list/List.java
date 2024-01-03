7d12b74d04961391f419a9e81c1b216eb14193b0
JKjiwon <jwkim.oa@gmail.com>
2024-01-03 18:17:20 +0900

    init

---
+++ src/main/java/algorithm/list/List.java
@@ -0,0 +1,18 @@
+package algorithm.list;
+
+public interface List<E> {
+
+    int size();
+
+    boolean add(E element);
+
+    E get(int index);
+
+    boolean remove(int index);
+
+    boolean remove();
+
+    boolean add(int index, E element);
+
+    boolean contains(E element);
+}
